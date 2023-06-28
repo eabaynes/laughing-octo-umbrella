1392160c184b6e5af7925110665ac28eaf05b269
Elliot <sbaynes1994@gmail.com>
2023-06-28 14:33:42 -0500

    update app.jsx to reflect project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "./layouts/header";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App">
+      <h1>Battleship</h1>
+      {/* add header under title */}
+      <Header />
+    </div>
+  );
 }
 
 export default App;
